4ef1834d45d27a2e4b7afdf9636cb633169f4d0a
Sai Neela Prajeet (Accenture) <v-saipr@microsoft.com>
2023-08-09 16:06:58 +0530

    resolving PR comments

--- src/Generator/Helper.cs
+++ src/Generator/Helper.cs
@@ -25,15 +25,15 @@ internal static class Helper
 
     public static int IndexOf<T>(this IReadOnlyList<T> self, T elementToFind)
     {
-        int i = 0;
+        int index = 0;
         foreach (T element in self)
         {
             if (Equals(element, elementToFind))
             {
-                return i;
+                return index;
             }
 
-            i++;
+            index++;
         }
 
         return -1;
